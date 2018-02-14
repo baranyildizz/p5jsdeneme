6e21738eb3f26e09a2540c5d89f3aff75d42e850
baranyildizz <30391869+baranyildizz@users.noreply.github.com>
2018-02-14 03:45:00 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,16 +3,13 @@
 // http://natureofcode.com/
 // Session 2: Array of Particles, multiple forces
 
-var value = 0;
 
 function setup(){
   createCanvas(800,800);
   background(0);
-   
-
 }
 
 function draw(){
   fill(255,0,0);
-  rect(mouseX, mouseY, 20, 20);
+  ellipse(mouseX, mouseY, 20, 10);
 }
